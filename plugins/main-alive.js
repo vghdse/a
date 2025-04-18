0399b5d4214c95758ac92c47e8016a352df80b0e
takudzwa07 <mrfrankofc17@gmail.com>
2025-04-18 17:42:09 +0200

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,6 +1,8 @@
 const { cmd } = require("../command");
 const moment = require("moment-timezone");
 const config = require("../config"); // Import config file
+const os = require("os");
+const { runtime } = require('../lib/functions');
 let botStartTime = Date.now();
 
 const ALIVE_IMG = "https://i.postimg.cc/3RgPr15t/IMG-20250305-WA0008.jpg";
@@ -28,6 +30,7 @@ cmd({
  *⏰ Time: ${harareTime}*
  *📆 Date: ${harareDate}*
  *🔋 Uptime: ${runtimeHours} hours, ${runtimeMinutes} minutes, ${runtimeSeconds} seconds*
+ *📀 Ram Usage* : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB 
 
 
  \`Status\`: *Subzero is online! 🤗🚀*
