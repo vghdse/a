d5874f603a8ac282fc6e7ac1c2bf2a7754f7902c
4strox <darexofx@gmail.com>
2025-05-09 22:28:38 +0200

    Update store.js

--- data/store.js
+++ data/store.js
@@ -1,4 +1,4 @@
-const config = require('./config')
+const config = require('../config')
 const { isJidBroadcast, isJidGroup, isJidNewsletter } = require(config.BAILEYS);
 const fs = require('fs/promises')
 const path = require('path')
