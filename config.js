9d878c1bd1f43e40183799ad984e3b9e0a19e499
4strox <darexofx@gmail.com>
2025-05-10 09:10:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
     BOT_NAME: process.env.BOT_NAME || "SUBZERO-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     REPO: process.env.REPO || "https://github.com/mrfrankofcc/SUBZERO-MD",  // Bot's GitHub repo
-    BAILEYS: process.env.BAILEYS || "@shizodevs/baileys",  // Bot's BAILEYS
+    BAILEYS: process.env.BAILEYS || "baileys-elite",  // Bot's BAILEYS
 
     // ===== OWNER & DEVELOPER SETTINGS =====
     OWNER_NUMBER: process.env.OWNER_NUMBER || "263719647303",  // Owner's WhatsApp number
