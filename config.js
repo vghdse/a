68c09b356298cad565758332838deee010b82634
4strox <darexofx@gmail.com>
2025-05-13 20:47:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const configPath = path.join(__dirname, "lib", "config.json");
-const configData = JSON.parse(fs.readFileSync(configPath, "utf8"));
+//const configPath = path.join(__dirname, "lib", "config.json");
+//const configData = JSON.parse(fs.readFileSync(configPath, "utf8"));
+const { getConfig } = require("./lib/configdb");
 
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -12,15 +13,15 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     // ===== BOT CORE SETTINGS =====
     SESSION_ID: process.env.SESSION_ID || "",  // Your bot's session ID (keep it secure)
-    PREFIX: configData.PREFIX || ".",  // Command prefix (e.g., "., / ! * - +")
-    BOT_NAME: process.env.BOT_NAME || configData.BOT_NAME || "SUBZERO-MD",  // Bot's display name
+    PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
+    BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "SUBZERO-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     REPO: process.env.REPO || "https://github.com/mrfrankofcc/SUBZERO-MD",  // Bot's GitHub repo
     BAILEYS: process.env.BAILEYS || "@shizodevs/baileys",  // Bot's BAILEYS
 
     // ===== OWNER & DEVELOPER SETTINGS =====
     OWNER_NUMBER: process.env.OWNER_NUMBER || "263719647303",  // Owner's WhatsApp number
-    OWNER_NAME: process.env.OWNER_NAME || configData.OWNER_NAME || "Mr Frank",           // Owner's name
+    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "Mr Frank",           // Owner's name
     DEV: process.env.DEV || "263719647303",                     // Developer's contact number
     DEVELOPER_NUMBER: '263719647303@s.whatsapp.net',            // Developer's WhatsApp ID
 
@@ -43,7 +44,7 @@ module.exports = {
     AUTO_VOICE: process.env.AUTO_VOICE || "false",              // Auto-send voice messages?
     AUTO_RECORDING: process.env.AUTO_RECORDING || "false",      // Auto-record voice notes?
     AUTO_TYPING: process.env.AUTO_TYPING || "false",            // Show typing indicator?
-    BOT_IMAGE: configData.BOT_IMAGE || "",  // Bot's "alive" image
+    BOT_IMAGE: getConfig("BOT_IMAGE") || "https://i.postimg.cc/XNTmcqZ3/subzero-menu.png",  // Bot's "alive" image
 
     // ===== SECURITY & ANTI-FEATURES =====
     ANTI_CALL: process.env.ANTI_CALL || "true",
